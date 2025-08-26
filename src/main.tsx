7c4c28ed60f18d8268a17f010714135fced3cca8
Kelbie <kevin@kelbie.me>
2025-08-26 21:33:44 +0100

    Import Tailwind styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import ParentSize from '@visx/responsive/lib/components/ParentSize';
 
 import Example from './App';
-import './sandbox-styles.css';
+import './index.css';
 
 const root = createRoot(document.getElementById('root')!);
 
