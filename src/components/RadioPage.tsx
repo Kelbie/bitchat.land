033ff3113d44cf61bc85a52555d565287c3d0026
Kelbie <kevin@kelbie.me>
2025-08-29 18:57:04 +0100

    Update RadioPage.tsx

--- src/components/RadioPage.tsx
+++ src/components/RadioPage.tsx
@@ -857,7 +857,7 @@ export function RadioPage({ searchText, theme }: RadioPageProps) {
                 <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z" />
               </svg>
               <p>Search for radio stations using a geohash</p>
-              <p className="text-sm mt-2">Example: "music in:dr5r"</p>
+              <p className="text-sm mt-2">Example: "in:gc"</p>
             </div>
           </div>
         )}
